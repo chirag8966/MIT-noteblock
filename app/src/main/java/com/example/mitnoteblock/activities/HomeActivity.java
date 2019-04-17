fb35f6cc350d24bac3913ce73fd379ac8b46c43a
chirag jain <chirag8966@gmail.com>
2019-04-17 21:32:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mitnoteblock/activities/HomeActivity.java
@@ -0,0 +1,58 @@
+package com.example.mitnoteblock.activities;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.mitnoteblock.R;
+import com.example.mitnoteblock.fragments.HomeFragment;
+
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener
+{
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        displayFragment(new HomeFragment());
+    }
+
+    private void displayFragment(Fragment fragment){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_area , fragment)
+                .commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment;
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.nav_fav:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.nav_set:
+                fragment = new HomeFragment();
+                break;
+
+            default:
+                fragment = new HomeFragment();
+                break;
+        }
+        displayFragment(fragment);
+        return true;
+    }
+}
